724705155e3667505bc0c4f767846e87be94d6da
hugo_pzz <hugo.ortz.04@gmail.com>
2025-05-26 17:03:04 -0600

    Initial commit

---
+++ src/test/java/mx/unam/aragon/thinkhub/ThinkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.thinkhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
